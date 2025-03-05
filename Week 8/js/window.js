4de4650b6bd798d0dac08c08bb87c40e0907ac83
Xavier Kyser <ac12b34u@gmail.com>
2025-03-05 16:50:22 -0500

    8

---
+++ Week 8/js/window.js
@@ -0,0 +1,12 @@
+console.log("Windows 7 is still the only OS to use");
+
+// alert("Missile is inbound to your exact location!");
+
+function getFavoritePeepFlavor() {
+  const flavor = prompt(" What's your favorite peep flavor?", "Regular");
+  console.log("Favorite flavor:", flavor);
+}
+function isTyGettingFlyers() {
+  const chooseToLeave = confirm("Are u getting the flyers?");
+  console.log("Ty is getting the flyers", chooseToLeave);
+}
